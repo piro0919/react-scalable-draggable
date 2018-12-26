e35e65c3565ee85cef894366b6464f4469e214b0
kohei.kawamura <piro.haniwa@gmail.com>
2018-12-26 15:33:27 +0900

    add react-router-dom

--- public/index.tsx
+++ public/index.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter
+    basename={
+      process.env.NODE_ENV === 'development' ? '' : '/react-scalable-draggable'
+    }
+  >
+    <Route component={App} path="/" />
+  </BrowserRouter>,
+  document.getElementById('app')
+);
